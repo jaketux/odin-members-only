a510e1af5daad9e4b49ec3ab7357b55c36668f15
jaketux <169253660+jaketux@users.noreply.github.com>
2025-07-06 07:49:48 +1000

    Odin Project Members Clubhouse - Users can create an account - Without member status, messages are anonymous. - Users can enter in the secret phrase "Odin" to be upgraded to Member status. - Admin users can delete messages.

--- db/populatedb.js
+++ db/populatedb.js
@@ -1,19 +1,51 @@
+require("dotenv").config();
+
+const environment = process.env.NODE_ENV || "development";
+
+const { Client } = require("pg");
+
 const SQL = `CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    username VARCHAR (255), first_name VARCHAR (255), last_name VARCHARR (255), member_status BOOLEAN NOT NULL DEFAULT FALSE;
+    username VARCHAR (255), 
+    first_name VARCHAR (255), 
+    last_name VARCHAR (255), 
+    member_status BOOLEAN NOT NULL DEFAULT FALSE, 
+    admin BOOLEAN
     );
-
-    INSERT INTO users (username, first_name, last_name, member_status)
-    VALUES ('jtux123', 'Jake', 'Tucker', true);
 
 
     CREATE TABLE IF NOT EXISTS messages (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    message VARCHAR ( 255 ), time VARCHAR (255), user_id INT REFERENCES users(id);
+    message VARCHAR (255), 
+    time VARCHAR (255), 
+    user_id INT REFERENCES users(id)
+    );`;
 
-    ALTER TABLE messages ADD COLUMN IF NOT EXISTS time VARCHAR;
-    ALTER TABLE messages ADD COLUMN IF NOT EXISTS user_id INT;
-    
+async function main() {
+  console.log("creating db");
+  const client = new Client({
+    ...(process.env.DATABASE_URL
+      ? { connectionString: process.env.DATABASE_URL }
+      : {
+          host: process.env.DB_HOST,
+          user: process.env.DB_USERNAME,
+          database: process.env.DB_USED,
+          password: process.env.DB_PASSWORD,
+          port: 5432,
+        }),
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done!");
+}
 
-
-)`;
+main()
+  .then(() => {
+    console.log("Seeding script completed");
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error("Seeding script failed: ", error);
+    process.exit(1);
+  });
